c870b28daab41c3c4b01539091f9a6dbed3d25fd
Jim wsl laptop Steffes <jimsteffes@outlook.com>
2023-11-13 02:40:47 -0600

    started global script

---
+++ html/global.js
@@ -0,0 +1,2 @@
+console.log('from github')
+console.log('https://raw.githubusercontent.com/wither7007/htmlTemplate/main/html/global.js')
---
+++ html/script.js
@@ -0,0 +1,2 @@
+console.log('this is my github script')
+console.log('https://raw.githubusercontent.com/wither7007/htmlTemplate/main/html/global.js')
