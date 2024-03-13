0613f7b1e01186bd43ff88f6d75d4c549144145f
Jim Steffes <wither7007@gmail.com>
2024-03-12 23:56:32 -0500

    write

--- html/script.js
+++ html/script.js
@@ -1,2 +1,3 @@
 console.log('this is my github script')
 console.log('https://raw.githubusercontent.com/wither7007/htmlTemplate/main/html/global.js')
+document.write('https://cdn.jsdelivr.net/gh/wither7007/htmlTemplate/html/script.js')
