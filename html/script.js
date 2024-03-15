87ff7794f4f80ed03fcbda6c10b3dc4b95dc86f4
Jim wsl laptop Steffes <jimsteffes@outlook.com>
2024-03-15 02:32:04 -0500

    no document write

--- html/script.js
+++ html/script.js
@@ -1,3 +1,7 @@
 console.log('this is my github script')
 console.log('https://raw.githubusercontent.com/wither7007/htmlTemplate/main/html/global.js')
-document.write('https://cdn.jsdelivr.net/gh/wither7007/htmlTemplate/html/script.js')
+console.log('')
+let mydeliver='https://cdn.jsdelivr.net/gh/wither7007/htmlTemplate/html/script.js'
+let myd=document.createElement('div')
+myd.textContent=mydeliver
+document.body.prepend(myd)
