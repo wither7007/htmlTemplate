07ae0d7ab3345e5f33d49e47d1d03ae49092b5bd
Jim Steffes <wither7007@gmail.com>
2024-11-24 12:03:09 -0600

    Update script.js

--- html/script.js
+++ html/script.js
@@ -1,6 +1,6 @@
 console.log('this is my github script')
 console.log('https://raw.githubusercontent.com/wither7007/htmlTemplate/main/html/global.js')
-console.log('')
+console.log('update')
 let mydeliver = 'https://cdn.jsdelivr.net/gh/wither7007/htmlTemplate/html/script.js'
 let myd = document.createElement('div')
 myd.textContent = mydeliver
