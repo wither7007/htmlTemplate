8a5b5a929b9fa9246091402244b6dd0aec579979
jim steffes <jimsteffes@outlook.com>
2024-11-24 12:16:38 -0600

    new test

---
+++ README.md
@@ -0,0 +1,3 @@
+An html templace
+also a jsdeliver
+https://cdn.jsdelivr.net/gh/wither7007/htmlTemplate@main/html/events.js
--- html/global.js
+++ html/global.js
@@ -1,2 +1,3 @@
 console.log('from github')
 console.log('https://raw.githubusercontent.com/wither7007/htmlTemplate/main/html/global.js')
+console.log('Sunday the 24th')
